58a185b39de462ddeed941b64b230071e63f9645
Johnatas Souza <109979241+John1Souza@users.noreply.github.com>
2024-02-05 16:01:57 -0300

    Update GetUser.java

--- src/basic.test/GetUser.java
+++ src/basic.test/GetUser.java
@@ -1,1 +1,19 @@
+package basic.test;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class GetUser{
+  public static void main(String[] args){
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("exercise-jpa");
+    EntityManager em = emf.createEntityManager();
+    
+    User user = em.find(User.class, 1);
+    System.out.println(user.getName());
+    
+    
+    em.close();
+    emf.close();
+  }
+}
