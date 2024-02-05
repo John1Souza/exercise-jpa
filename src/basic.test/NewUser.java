7116d0ecb68325c7da924d48cf0a236688cc9085
Johnatas Souza <109979241+John1Souza@users.noreply.github.com>
2024-02-05 15:47:47 -0300

    Update NewUser.java
    configuring NewUser class

--- src/basic.test/NewUser.java
+++ src/basic.test/NewUser.java
@@ -1,5 +1,17 @@
 package basic.test;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 public class NewUser{
-  
+  public static void main(String[] args){
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("exercise-jpa");
+    EntityManager em = emf.createEntityManager();
+    
+    User newUser = new User("Djalma","djalma@lanche.com.br");
+    
+    em.persist(newUser);
+    em.close();
+    emf.close();
+  }
 }
