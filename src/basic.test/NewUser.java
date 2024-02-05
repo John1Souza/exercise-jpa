b4ae50d4dfaf8a665c5a043b2847b64cbe619e7b
Johnatas Souza <109979241+John1Souza@users.noreply.github.com>
2024-02-05 15:57:19 -0300

    Update NewUser.java

--- src/basic.test/NewUser.java
+++ src/basic.test/NewUser.java
@@ -9,6 +9,11 @@ public class NewUser{
     EntityManager em = emf.createEntityManager();
     
     User newUser = new User("Djalma","djalma@lanche.com.br");
+    //User("Rodrigo","rodrigo@lanche.com.br")
+    //User("Andre","andre@lanche.com.br")
+    //User("Carlos","carlos@lanche.com.br")
+    //User("Lulu","aulu@lanche.com.br")
+    //User("Arthur","arthur@lanche.com.br")   
 
     
     em.getTransaction().begin();
