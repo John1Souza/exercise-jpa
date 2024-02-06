97e2896c4659578c1655b2e17ea6a8fae01ee07e
Johnatas Souza <109979241+John1Souza@users.noreply.github.com>
2024-02-06 09:41:34 -0300

    Update Product.java

--- src/basic.model/Product.java
+++ src/basic.model/Product.java
@@ -1,12 +1,17 @@
 package basic.model;
 
 @Entity
+@Table(name = "tb_products", schema = "java_course")
 public class Product {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  @Column(name = "prod_name", length = 200, nullable = false)
   private String name;
+
+  @Column(name = "prod_price", nullable = false, precision = 11, scale = 2) // Precision = tamanho máximo do número, Scale = quantos números após a vírguala EX: 123456789,00
   private Double price;
 
   public Product(){
