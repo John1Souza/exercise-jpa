519d2f0059949ee3586fa61f783baf486a479cd4
Johnatas Souza <109979241+John1Souza@users.noreply.github.com>
2024-02-06 09:25:51 -0300

    Update RemoveUser.java

--- src/basic.test/RemoveUser.java
+++ src/basic.test/RemoveUser.java
@@ -1,1 +1,23 @@
+package basic.test;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+public class GetUser{
+  public static void main(String[] args){
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("exercise-jpa");
+    EntityManager em = emf.createEntityManager();
+    
+    User user = em.find(User.class, 8L);
+    if(user != null){
+      em.getTransaction().begin();
+      em.remove(user);
+      em.getTransaction().commit();
+    }
+    
+    em.close();
+    emf.close();
+  }
+}
