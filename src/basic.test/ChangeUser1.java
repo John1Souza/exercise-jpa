befd78f1e2601e0ddf7549c4468bd72ef973ebc3
Johnatas Souza <109979241+John1Souza@users.noreply.github.com>
2024-02-06 08:58:20 -0300

    Update ChangeUser1.java

--- src/basic.test/ChangeUser1.java
+++ src/basic.test/ChangeUser1.java
@@ -1,1 +1,25 @@
+package basic.test;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+public class GetUser{
+  public static void main(String[] args){
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("exercise-jpa");
+    EntityManager em = emf.createEntityManager();
+
+    em.getTransaction().begin();
+
+    User user = em.fin(User.class, 7L);
+    user.setName("Leonardo");
+    user.setEmail("leonardo@lanche.com.br");
+
+    em.merge(user);
+    
+    em.getTransaction().commit();
+    em.close();
+    emf.close();
+  }
+}
