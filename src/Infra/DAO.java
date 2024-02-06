a86dbcc57d61d25a1201fc618df848a21d0f7c89
Johnatas Souza <109979241+John1Souza@users.noreply.github.com>
2024-02-06 10:03:02 -0300

    Update DAO.java

--- src/Infra/DAO.java
+++ src/Infra/DAO.java
@@ -2,4 +2,62 @@ package infra;
 
 public class DAO<E> {
   
+  private static EntityManagerFactory emf;
+  private EntityManager em;
+  private Class<E> classe;
+
+  static {
+    try{
+      emf = Persistence.createEntityManagerFactory("exercise-jpa");
+    } catch(Exception e) {
+      // login -> log4j
+    }
+  }
+
+  public DAO(){
+    this(null);
+  }
+  
+  public DAO(Class<E> classe){
+    this.classe = classe;
+    em = emf.createEntityManager();
+  }
+
+  public DAO<E> openTransaction(){
+    em.getTransaction().begin();
+    return this;
+  }
+
+  public DAO<E> closeTransaction(){
+    em.getTransaction().commit();
+    return this;
+  }
+
+  public DAO<E> addTransaction(E entity){
+    em.persist(entity);
+    return this;
+  }
+
+  public DAO<E> addAtomicTransaction(E entity){   
+    return  this.openTransaction().addTransaction(E entity).closeTransaction();
+  }
+
+  public List<E> getAll(){
+    return this.getAll(10, 0);
+  }
+  public List<E> getAll(int limit, int offset){
+    if(classe == null){
+      throw new UnsopportedOperationException("null Class.");
+    }
+
+    String jpql = "select e from " + class.getName() + " e"; 
+    TypedQuery<E> query = em.createdQuery(jpql, classe);
+    query.setMaxResults(limit);
+    query.setFirstResult(offset);
+    return query.getResultList();
+  }
+
+  public void closeDAO(){
+    em.close();
+  }
 }
