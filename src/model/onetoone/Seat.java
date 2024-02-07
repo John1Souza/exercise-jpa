54987a92e88301f31d2d2a796468afca88d51b8a
Johnatas Souza <109979241+John1Souza@users.noreply.github.com>
2024-02-07 17:45:21 -0300

    Update Seat.java

--- src/model/onetoone/Seat.java
+++ src/model/onetoone/Seat.java
@@ -1,1 +1,18 @@
+package model.onetoone;
 
+import javax.persistence.Entity;
+import javax.persistence.GenerationValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "seats")
+public class Seat {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+}
