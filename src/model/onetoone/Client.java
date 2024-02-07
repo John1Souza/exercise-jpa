e580263ed086c39573281bf4b48085434bc1934e
Johnatas Souza <109979241+John1Souza@users.noreply.github.com>
2024-02-07 17:43:26 -0300

    Update Client.java

--- src/model/onetoone/Client.java
+++ src/model/onetoone/Client.java
@@ -1,1 +1,13 @@
+package model.onetoone;
 
+import javax.persistence.Entity;
+
+@Entity
+  public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+  }
