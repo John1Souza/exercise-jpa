d1a3a32d50e3309a0fbf18b44877269d4a2eb2ff
Johnatas Souza <109979241+John1Souza@users.noreply.github.com>
2024-02-07 16:12:53 -0300

    Update NewProduct.java

--- src/basic.test/NewProduct.java
+++ src/basic.test/NewProduct.java
@@ -5,8 +5,8 @@ public class NewProduct {
     Product product = new Product("Pen", 7.45);
     
     DAO<Product> dao = new DAO<>(Product.class);
-    dao.openTransaction().add(product).closeTransaction().close();
-    //dao.atomicInclude(product).close();
+    dao.openTransaction().addTransaction(product).closeTransaction().close();
+    //dao.addAtomicTransaction(product).close();
 
     System.out.println("Product ID: " + product.getId());
   }
