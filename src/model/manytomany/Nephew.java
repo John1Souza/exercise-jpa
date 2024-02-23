5dd5ee94efdfa514108b2248be4e64bb054536d5
Johnatas Souza <109979241+John1Souza@users.noreply.github.com>
2024-02-23 17:19:26 -0300

    some changes

--- src/model/manytomany/Nephew.java
+++ src/model/manytomany/Nephew.java
@@ -1,10 +1,14 @@
 package model.manytomany;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Nephew {
@@ -14,5 +18,8 @@ public class Nephew {
     private Long id;
 
     private String name;
+
+    @ManyToMany(mappedBy = "nephews")
+    private List<Uncle> uncles = new ArrayList<>();
 
 }
--- src/model/manytomany/Uncle.java
+++ src/model/manytomany/Uncle.java
@@ -1,10 +1,13 @@
 package model.manytomany;
 
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Uncle {
@@ -12,6 +15,8 @@ public class Uncle {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+
     private String name;
 
     @ManyToMany
